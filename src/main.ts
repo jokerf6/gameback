ef2497f543db29fb9f56194b3493fb7061d9f9e4
jokerf6 <fhakem75@gmail.com>
2023-08-23 14:00:41 +0300

    add

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import { NestFactory } from "@nestjs/core";
+import { SwaggerInit } from "./util/SwaggerConfig";
+import { AppModule } from "./app.module";
+import { HttpExceptionFilter } from "./util/http-exception.filter";
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.enableCors();
+  app.setGlobalPrefix("/");
+  if (process.env.NODE_ENV === "development") {
+    app.enableCors({
+      origin: "*",
+      methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+      allowedHeaders: "*",
+    });
+  }
+  SwaggerInit.init(app);
+  await app.listen(4000);
+  console.log(`Application is running on: ${4000}`);
+  console.log(`Swagger Docomentation On: ${await app.getUrl()}/api/v1/docs`);
+}
+
+bootstrap();
